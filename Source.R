d32ad819dad9250f53caa1e6ac2320fab576dbbb
Karan Bhanot <bhanotkaran22@gmail.com>
2020-03-30 01:06:47 -0400

    A better way to import and manage packages with comments

--- Source.R
+++ Source.R
@@ -1,3 +1,22 @@
+##### Load packages
+
+# List of packages used:
+# - dplyr
+# - geojsonio
+# - htmltools
+# - leaflet
+# - leaflet.extras
+# - shiny
+# - tidyverse
+dependencies <- c("shiny", "dplyr", "geojsonio", "htmltools", "leaflet", "leaflet.extras", "tidyverse")
+
+# Check and install packages not yet available
+install.dependencies <- dependencies[!(install.dependencies %in% installed.packages()[, "Package"])]
+if (length(install.dependencies) > 0) {
+  install.packages(install.dependencies)
+}
+
+# Load all packages
 library(dplyr)
 library(geojsonio)
 library(htmltools)
