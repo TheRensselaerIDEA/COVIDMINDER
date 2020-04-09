956648499db4c15843927d4ec9fd04d151864b71
Jose Figueroa <josefigueroa168@gmail.com>
2020-04-09 13:28:50 -0400

    remove fixed footer

--- app.R
+++ app.R
@@ -168,7 +168,7 @@ ui <-
            )
   ),
   tags$br(),
-  footer = fluidRow(class = "navbar navbar-default navbar-fixed-bottom footer", 
+  footer = fluidRow(class = "navbar navbar-default footer", 
                     column(6,
                            HTML("<b>ABOUT: </b>
                                 <b>COVIDMINDER</b> is an open source interactive application 
