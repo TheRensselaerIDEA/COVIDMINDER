a4ec9e4bdbfebb88ad3c4f708fb117100aa78c01
John Erickson <olyerickson@gmail.com>
2020-04-09 11:12:17 -0400

    Added diabetes and fixed -Inf issue

--- modules/preprocessing.R
+++ modules/preprocessing.R
@@ -22,6 +22,10 @@ hosp_beds_ldi <- unlist(lapply(provider_capacity$p_hosp_beds, FUN=function(x){-l
 provider_capacity <- data.frame(provider_capacity, hosp_beds_ldi)
 provider_capacity <- provider_capacity[match(states$NAME, provider_capacity$NAME),]
 provider_capacity <- provider_capacity[1:51,]
+
+provider_capacity <- provider_capacity %>% 
+  mutate(hosp_beds_ldi = replace(hosp_beds_ldi, hosp_beds_ldi < -5, -5)) 
+
 states <- data.frame(states, "hosp_beds_ldi"=provider_capacity$hosp_beds_ldi) # Append to states
 
 ## COVID-19 Testing fixing
@@ -47,6 +51,9 @@ state_covid_testing <- state_covid_testing[match(states$NAME, state_covid_testin
 
 state_covid_testing <- state_covid_testing[1:51,]
 
+state_covid_testing <- state_covid_testing %>% 
+  mutate(tests_ldi = replace(tests_ldi, tests_ldi < -5, -5)) 
+
 states <- data.frame(states, "tests_per_1000"=state_covid_testing$tests_per_1000) # Append to states
 states <- data.frame(states, "tests_ldi"=state_covid_testing$tests_ldi) # Append to states
 states <- data.frame(states, "Population"=state_covid_testing$Population) # Append to states (reference)
@@ -68,6 +75,10 @@ older_at_risk_ldi <- unlist(lapply(at_risk_adults$p_older_at_risk_adults, FUN=fu
 
 at_risk_adults <- data.frame(at_risk_adults, older_at_risk_ldi)
 
+# at_risk_adults <- at_risk_adults %>% 
+#   mutate(at_risk_adults = replace(at_risk_adults, at_risk_adults < -5, -5)) %>%
+#   mutate(older_at_risk_ldi = replace(older_at_risk_ldi, older_at_risk_ldi < -5, -5)) 
+
 # RE-order to match states ordering
 at_risk_adults <- at_risk_adults[match(states$NAME, at_risk_adults$NAME),]
 
@@ -85,6 +96,9 @@ pUS.5 <- as.numeric(cardio_deaths_2017[which(cardio_deaths_2017$NAME=="United St
 cardio_death_rate_ldi <- unlist(lapply(cardio_deaths_2017$cardio_deaths_p_100000, FUN=function(x){-log(pUS.5/x)}))
 
 cardio_deaths_2017 <- data.frame(cardio_deaths_2017, cardio_death_rate_ldi)
+
+cardio_deaths_2017 <- cardio_deaths_2017 %>% 
+  mutate(cardio_death_rate_ldi = replace(cardio_death_rate_ldi, cardio_death_rate_ldi < -5, -5)) 
 
 # RE-order to match states ordering
 cardio_deaths_2017 <- cardio_deaths_2017[match(states$NAME, cardio_deaths_2017$NAME),]
@@ -104,6 +118,9 @@ death_rate_ldi <- unlist(lapply(covid_data_states$p_death_rate, FUN=function(x){
 
 covid_data_states <- data.frame(covid_data_states, death_rate_ldi)
 
+covid_data_states <- covid_data_states %>% 
+  mutate(death_rate_ldi = replace(death_rate_ldi, death_rate_ldi < -5, -5)) 
+
 # RE-order to match states ordering
 covid_data_states <- covid_data_states[match(states$NAME, covid_data_states$NAME),]
 
@@ -116,7 +133,8 @@ states <- data.frame(states, "covid_death_rate"=covid_data_states$p_death_rate) 
 # NY specific calculations: Death Rates
 pNY.6.deaths <- sum(NY.data$deaths)/sum(NY.data$Population)
 pNY.6.cases <- sum(NY.data$cases)/sum(NY.data$Population)
-  
+pNY.6.diabetes <- as.numeric(NY_counties_diabetes[1,"pct_Adults_with_Diabetes"])
+
 NY.data <- transform(NY.data, death_rate = deaths/Population)
 #NY.data <- transform(NY.data, case_rate = cases/Population) # We already do this
 
@@ -127,3 +145,13 @@ NY.data <- NY.data %>%
 NY.data$death_rate_ldi <- unlist(lapply(NY.data$death_rate, FUN=function(x){-log(pUS.6/x)})) # vs UR rate
 
 NY.data$case_rate_ldi <- unlist(lapply(NY.data$case_rate, FUN=function(x){-log(pNY.6.cases/x)}))
+
+NY.data$diabetes_ldi <- unlist(lapply(NY.data$pct_Adults_with_Diabetes, FUN=function(x){-log(pNY.6.diabetes/x)}))
+
+# Clean up the ranges
+NY.data <- NY.data %>% 
+  mutate(death_rate_ldi = replace(death_rate_ldi, death_rate_ldi < -5, -5)) %>%
+  mutate(case_rate_ldi = replace(case_rate_ldi, case_rate_ldi < -5, -5)) %>%
+  mutate(diabetes_ldi = replace(diabetes_ldi, diabetes_ldi < -5, -5)) 
+  
+
