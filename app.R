bcc6823cc5b6f6dda3a63980418d6cc89ea5a3fc
John Erickson <olyerickson@gmail.com>
2020-04-16 11:01:37 -0400

    Corrected another typo in LH column, heart disease

--- app.R
+++ app.R
@@ -182,14 +182,14 @@ ui <-
                                 Heart disease patients at increased risk of contracting and dying from COVID-19, 
                                 so areas with a history of higher heart disease mortality may face increased COVID-19 burdens. 
                                 Furthermore, some ethnic groups have higher mortality rates due to heart disease than other groups. <br><br>
-                               The rate of deaths due to heart disease per 100k (BNH) in a state is<br>
+                               The rate of deaths due to heart disease (black non-hispanic) per 100k in a state is<br>
                                <div>&nbsp;&nbsp;&nbsp;<span style='background: #BD0026; border-radius: 50%; font-size: 11px; opacity: 0.7;'>&nbsp&nbsp&nbsp&nbsp</span><strong> Higher</strong> than US avg. rate for disparity index &gt; 0.2</div>
                                <div>&nbsp;&nbsp;&nbsp;<span style='background: #ffffff; border-radius: 50%; font-size: 11px; opacity: 0.7;'>&nbsp&nbsp&nbsp&nbsp</span><strong> About equal</strong> to US avg. rate for -0.2 &lt;disparity index &lt; 0.2</div>
                                <div>&nbsp;&nbsp;&nbsp;<span style='background: #253494; border-radius: 50%; font-size: 11px; opacity: 0.7;'>&nbsp&nbsp&nbsp&nbsp</span><strong> Lower</strong> than US avg. rate for disparity index &lt; -0.2</div>
                                <i>Darker shades indicate greater disparity.</i><br><br>
                                
-                               <strong>Heart Disease Death Rate</strong> = number of heart disease deaths per 100K population (BNH) <br>
-                               <strong>Heart Disease Death Disparity Index</strong> = log(Heart Disease Death Rate (BNH) in state/average Heart Disease Death Rate in US)<br>
+                               <strong>Heart Disease Death Rate (BNH)</strong> = number of heart disease deaths (black non-hispanic) per 100K population <br>
+                               <strong>Heart Disease Death Disparity Index (BNH)</strong> = log(Heart Disease Death Rate (BNH) in state/average Heart Disease Death Rate in US)<br>
                                <strong>Date:</strong> 2015<br><br>
                                
                                <b>DATA SOURCE:</b> <a href='https://sortablestats.cdc.gov/#/indicator'>CDC</a><br>
