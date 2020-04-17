12b6eb64eeee1568a253c1cd983ebe90e7379ab9
Jose Figueroa <josefigueroa168@gmail.com>
2020-04-16 22:25:47 -0400

    Change id representation

--- www/style.js
+++ www/style.js
@@ -1,14 +1,25 @@
-
-var sidebars = ["sidebar_us_mort", "sidebar_us_test", "sidebar_us_hosp", "sidebar_us_db", "sidebar_us_cardio", "sidebar_ny_mort", "sidebar_ny_cases", "sidebar_ny_CoT", "sidebar_ny_det", "sidebar_ny_CoT_rates"];
-var mainpanels = ["#mainpanel_us_mort", "#mainpanel_us_test", "#mainpanel_us_hosp", "#mainpanel_us_db", "#mainpanel_us_cardio",  "#mainpanel_ny_mort", "#mainpanel_ny_cases", "#mainpanel_ny_CoT", "#mainpanel_ny_det", "#mainpanel_ny_CoT_rates"];
+// ids key: value pairs represent a corresponding sidebar: mainpanel
+// resize iterates through and 'snaps' mainpanel to sidepbars height. 
+var ids = {
+  "sidebar_us_mort": "#mainpanel_us_mort",
+  "sidebar_us_test": "#mainpanel_us_test",
+  "sidebar_us_hosp": "#mainpanel_us_hosp",
+  "sidebar_us_db": "#mainpanel_us_db",
+  "sidebar_us_cardio": "#mainpanel_us_cardio",
+  "sidebar_ny_mort": "#mainpanel_ny_mort",
+  "sidebar_ny_cases": "#mainpanel_ny_cases",
+  "sidebar_ny_CoT": "#mainpanel_ny_CoT", 
+  "sidebar_ny_det": "#mainpanel_ny_det", 
+  "sidebar_ny_CoT_rates": "#mainpanel_ny_CoT_rates"
+};
 
 function resize_plot() {
-  for (let i=0; i<sidebars.length; i++) {
-  var sidebar1 = document.getElementById(sidebars[i]);
+  for (var key in ids) {
+  var sidebar1 = document.getElementById(key);
   if(sidebar1) {
     var positionInfo = sidebar1.getBoundingClientRect();
     var height = positionInfo.height;
-    $(mainpanels[i]).height(height);
+    $(ids[key]).height(height);
   }
   }
 }
