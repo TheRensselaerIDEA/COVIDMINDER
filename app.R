91bc3b975c3f4dece6ee740714c0b3679771e3ec
Jose Figueroa <josefigueroa168@gmail.com>
2020-04-22 09:51:57 -0400

    Added changes to rates over time

--- app.R
+++ app.R
@@ -158,12 +158,14 @@ ui <-
                                        label = "NY Regions",
                                        choices = cbind(c("All Regions"), NY_counties_regions$Region),
                                        selected = "All Counties"),
-                           plotOutput(outputId = "NY.cases.TS", height="90%", 
+                           tags$div(class = "NY_case_plots",
+                           plotOutput(outputId = "NY.cases.TS", height="100%", 
                                       click = clickOpts(id ="NY.cases.TS_click"),
                                       dblclick = "NY.cases.TS_dblclick",
                                       brush = brushOpts(
                                         id = "NY.cases.TS_brush",
                                         resetOnNew = TRUE)),
+                           ),
                            HTML("<div style='font-size:80%;line-height:1.3;'>
                                 <br>To zoom plot, click and drag, then double-click in select box<br>
                                 To un-zoom, double-click in plot<br>
@@ -191,12 +193,18 @@ ui <-
                    width=4),
                  
                  mainPanel(id = "mainpanel_ny_CoT_rates",
-                           plotOutput(outputId = "NY.cases.TS.rates", height="90%",
+                           selectInput(inputId = "NYRegion.rates",
+                                       label = "NY Regions",
+                                       choices = cbind(c("All Regions"), NY_counties_regions$Region),
+                                       selected = "All Counties"),
+                           tags$div(class = "NY_case_plots",
+                           plotOutput(outputId = "NY.cases.TS.rates", height="100%",
                                       click = clickOpts(id ="NY.cases.TS.rates_click"),
                                       dblclick = "NY.cases.TS.rates_dblclick",
                                       brush = brushOpts(
                                         id = "NY.cases.TS.rates_brush",
                                         resetOnNew = TRUE)),
+                           ),
                            HTML("<div style='font-size:80%;line-height:1.3;'>
                                 <br>To zoom plot, click and drag, then double-click in select box<br>
                                 To un-zoom, double-click in plot<br>
@@ -951,7 +959,12 @@ server <- function(input, output, session) {
   
   output$NY.cases.TS.rates <- renderPlot({
     # browser()
-    
+    selected.region <- input$NYRegion.rates
+    select.size <- 2
+    if (selected.region == "All Regions") {
+      selected.region <- NY_counties_regions$Region
+      select.size <- 1
+    }
     highlight_points <- covid_NY_TS_plot.cases %>% 
       dplyr::filter( 
           County == "Albany" & date == as.Date("2020-03-26") |
@@ -1037,7 +1050,9 @@ server <- function(input, output, session) {
         ) +
       scale_x_datetime(date_breaks = "1 week", date_minor_breaks = "1 day", date_labels = "%b %d") +
       ylab("Cases per 100K Population") + 
-      ggtitle("New York State COVID-19 Cases per 100K Population by County (Mar-Apr 2020)")  + 
+      ggtitle("New York State COVID-19 Cases per 100K Population by County (Mar-Apr 2020)")  +   
+        gghighlight(NY_counties_regions[NY_counties_regions$County == County,"Region"] %in% selected.region, use_direct_label=FALSE) +
+        geom_line(size=select.size) + 
       geom_label_repel(data=highlight_points,  aes(label=County), segment.color="black", force=10) + 
       coord_cartesian(xlim = ranges2$x, ylim = ranges2$y, expand = FALSE) +
         geom_vline(aes(xintercept=as_datetime("2020-03-20"), linetype="Gov. Cuomo issues stay-at-home order"), color = "black") + 
