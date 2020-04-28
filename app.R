d5d2bda0cc8385aebc6936c5bf0d4323d5a65221
John Erickson <olyerickson@gmail.com>
2020-04-28 16:30:00 -0400

    Implemented comment icon

--- app.R
+++ app.R
@@ -15,8 +15,9 @@ footer_text <- "<br><div style='font-size: 80%;'><b>COVIDMINDER analysis and vis
                                 <b>COVIDMINDER</b> is an open source project implemented on the <a href='https://shiny.rstudio.com/'>R Shiny platform</a>;
                                 see the <a href='https://github.com/TheRensselaerIDEA/COVIDMINDER'>COVIDMINDER github</a>
                                 for more information. <br><br>
+                                <a href='https://forms.gle/8LwiYAVXXN7mu9wR6'><img src='comment.png' style='float:left;width:40px;padding-right:2px;' ></a>
                                 Thanks for using <b>COVIDMINDER!</b> Please take a few moments 
-                                to fill out our short <a href='https://forms.gle/8LwiYAVXXN7mu9wR6'>comments form.</a><br>
+                                to fill out our short <a href='https://forms.gle/8LwiYAVXXN7mu9wR6'>comments form.</a><br><br>
                                 <i><a href='https://info.rpi.edu/statement-of-accessibility'>Rensselaer Statement 
                                 of Accessibility</a></i></div>"
 
@@ -25,7 +26,8 @@ whatisit_text <-"<div style='font-size:80%;line-height:1.3;'><strong>COVIDMINDER
                                 effects of COVID-19. Social and Economic Determinants are pre-existing risk factors that impact 
                                 COVID-19 outcomes. Mediations are resources and programs used to combat the pandemic.</div><br>"
 
-comments_link <-"<div style='font-size:80%;line-height:1.3;'>Thanks for using <b>COVIDMINDER!</b> Please take a few moments to fill out our short <a href='https://forms.gle/8LwiYAVXXN7mu9wR6'>comments form.</a></div>"
+comments_link <-"<div style='font-size:80%;line-height:1.3;'>Thanks for using <b>COVIDMINDER!</b> Please take a few moments to fill 
+                                out our short <a href='https://forms.gle/8LwiYAVXXN7mu9wR6'>comments form.</a></div>"
 
 # For URL parameterization
 url1 <- url2 <- ""
