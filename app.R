21481778764bef2c7227c0815978565a78733925
Jose Figueroa <josefigueroa168@gmail.com>
2020-07-15 15:36:15 -0400

    Daily Update july-15

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ sourceDir("modules")
 sourceDir("modules/shiny/R")
 
 
-update_date <- "07-14-2020" # makes it easy to change all occurances when we update
+update_date <- "07-15-2020" # makes it easy to change all occurances when we update
 
 moving.avg.window <- 7 # WARNING: Behavior for moving.avg.window > number of report dates for a region is undefined.
                        # (i.e. a 20 day window if Catskill Region has 19 report dates.)
@@ -49,7 +49,7 @@ url1 <- url2 <- ""
 #### UI Code ####
 ui <- 
   tagList(
-    tags$html(lang = "en"),
+    tags$html(lang = "en-us"),
     tags$head(includeHTML("www/analytics.html")),
     navbarPage(
       id="tab",
