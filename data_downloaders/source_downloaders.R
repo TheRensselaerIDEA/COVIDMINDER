9f3b719a18fb6095ef45f1de4ba26e21d79f0dc8
Jose Figueroa <josefigueroa168@gmail.com>
2020-07-20 15:15:02 -0400

    Daily update july20

--- data_downloaders/source_downloaders.R
+++ data_downloaders/source_downloaders.R
@@ -7,5 +7,5 @@ source("data_downloaders/usa_facts_dl.R")
 source("data_downloaders/jhu_ts_downloader.R")
 source("data_downloaders/jhu_ny_ts_downloader.R")
 source("data_downloaders/cdc_downloader.R")
-source("data_downloaders/owid_downloader.R")           # Run for up to date testing numbers.
+source("data_downloaders/owid_downloader.R")             # Run for up to date testing numbers.
 # source("data_downloaders/jhu_ts_downloader_malik.R")   # Experimental; Malik's structures
