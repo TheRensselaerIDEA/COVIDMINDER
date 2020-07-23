c56c879348101280cff21b749ec69061fe3c73d3
Jose Figueroa <josefigueroa168@gmail.com>
2020-07-22 20:01:41 -0400

    Update date

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ source("modules/gt_gen.R")
 sourceDir("modules/shiny/R")
 
 
-update_date <- "07-20-2020" # makes it easy to change all occurances when we update
+update_date <- "07-21-2020" # makes it easy to change all occurances when we update
 
 moving.avg.window <- 7 # WARNING: Behavior for moving.avg.window > number of report dates for a region is undefined.
                        # (i.e. a 20 day window if Catskill Region has 19 report dates.)
