76752361c1e407ed411787db08ad36730f7390c9
brianhotopp <brihoto@gmail.com>
2020-07-30 11:58:19 -0400

    daily update 7-30-20

--- data_downloaders/cdc_downloader.R
+++ data_downloaders/cdc_downloader.R
@@ -30,14 +30,19 @@ todays_raw_racial_data <- todays_raw_racial_data %>%
 todays_raw_racial_data <- todays_raw_racial_data %>%
   mutate(indicator = replace(indicator, indicator == "Distribution of COVID-19 deaths (%)", "deaths_pct")) %>%
   mutate(indicator = replace(indicator, indicator == "Weighted distribution of population (%)", "wd_pop_pct")) %>%
-  mutate(indicator = replace(indicator, indicator == "Unweighted distribution of population (%)", "un_pop_pct"))
+  mutate(indicator = replace(indicator, indicator == "Unweighted distribution of population (%)", "un_pop_pct")) %>%
+  mutate(indicator = replace(indicator, indicator == "Count of COVID-19 deaths", "deaths_num")) 
 
 # Transform to wide
 todays_raw_racial_data.wide <- todays_raw_racial_data %>%
-  pivot_wider(names_from = indicator, values_from = 4:9)
+  #pivot_wider(names_from = indicator, values_from = 6:11)
+  pivot_wider(names_from = indicator, values_from = c(nhw, nhbaa, nhaian,  nhapi,     hlt,  other))
 
 # Make backup of existing data
-write_csv(read_csv("data/csv/states_cdc_racial_wide.csv"),"data/csv/states_cdc_racial_wide.csv.bak")
-
+yesterday_data <- read_csv("data/csv/states_cdc_racial_wide.csv")
+write_csv(yesterday_data,"data/csv/states_cdc_racial_wide.csv.bak")
 # write out new dataframe to file system 
 write_csv(todays_raw_racial_data.wide,"data/csv/states_cdc_racial_wide.csv")
+
+write_csv(todays_raw_racial_data.wide,"data/csv/states_cdc_racial_wide.csv.bak")
+  
