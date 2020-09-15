4617988b1381bcb9dc50ccf5c0872922b75fec92
brianhotopp <brihoto@gmail.com>
2020-09-14 20:06:27 -0400

    daily-update-2020-09-14

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ source("modules/gt_gen.R")
 sourceDir("modules/shiny/R")
 
 
-update_date <- "2020-09-13"
+update_date <- "2020-09-14"
 
 
 moving.avg.window <- 7 # WARNING: Behavior for moving.avg.window > number of report dates for a region is undefined.
