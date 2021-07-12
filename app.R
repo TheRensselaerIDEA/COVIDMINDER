c1edf3988570775656c77c780163ee314805a941
John Erickson <olyerickson@gmail.com>
2021-07-12 11:17:34 -0400

    Daily Update: 12 Jul 2021

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ source("modules/leaflet_gen.R")
 source("modules/gt_gen.R")
 sourceDir("modules/shiny/R")
 
-update_date <- "2021-06-23"
+update_date <- "2021-07-12"
 
 moving.avg.window <-
   7 # WARNING: Behavior for moving.avg.window > number of report dates for a region is undefined.
